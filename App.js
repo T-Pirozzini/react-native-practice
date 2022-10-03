d72c3412252673e022cacdda5d8afe4241bbb7d5
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-10-03 15:32:57 -0700

    initial project set up

--- App.js
+++ App.js
@@ -5,6 +5,15 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.bold}>Hello World!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>lorem ipsum <Text>test</Text> dolor isit amet.</Text>
+        <Text>lorem ipsum dolor isit amet.</Text>
+        <Text>lorem ipsum dolor isit amet.</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +26,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,    
+  },
+  boldText: {
+    fontWeight: 'bold',
+  }
 });
