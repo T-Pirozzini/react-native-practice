1abafa98b09b4bdba33f19ed3b10d5ea794038bf
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-10-06 14:45:20 -0700

    wrap app in TouchableWithoutFeedback and then dismiss keyboard onPress

--- practice/todo/App.js
+++ practice/todo/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
@@ -18,7 +18,6 @@ export default function App() {
   }
 
   const submitHandler = (text) => {
-
     if(text.length > 3) {
       setTodos((prevTodos) => {
         return [
@@ -31,24 +30,27 @@ export default function App() {
         {text: 'Understood', onPress: () => console.log('alert closed')}
       ]);
     }
-
   }
 
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        <AddTodo submitHandler={submitHandler} />
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({ item }) => (
-              <TodoItem item={item} pressHandler={pressHandler}/>
-            )}
-          />          
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();      
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler}/>
+              )}
+            />          
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
