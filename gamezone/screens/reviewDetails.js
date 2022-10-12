f87c558dbc4c99917b9f30638abee1648c10538c
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-10-12 15:29:53 -0700

    add press handlers to navigate between screens

--- gamezone/screens/reviewDetails.js
+++ gamezone/screens/reviewDetails.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text, Button} from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
+
+  const pressHandler = () => {
+    // navigation.navigate("Home")
+    navigation.goBack();
+  }
+
   return (
     <View style={globalStyles.container}>
       <Text>ReviewDetails Screen</Text>
+      <Button title="back to homescreen" onPress={pressHandler} />
     </View>
   )
 }
